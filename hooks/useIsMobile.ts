3d8fbe4e9e89ec89c25429349516888a2d8929e5
Lorenzo Pappalardo <lorenzopappalardo7@gmail.com>
2022-10-08 17:47:54 +0200

    Added hook to determine device type

---
+++ hooks/useIsMobile.ts
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react';
+
+const useIsMobile = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    function onResize() {
+      setIsMobile(window.innerWidth < 600);
+    }
+
+    window.addEventListener('resize', onResize);
+
+    return () => {
+      window.removeEventListener('resize', onResize);
+    };
+  }, []);
+
+  return isMobile;
+};
+
+export default useIsMobile;
