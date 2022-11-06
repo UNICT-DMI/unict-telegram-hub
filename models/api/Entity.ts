27d703e18eb4c98c86deacf14f9d1a89a19ee4bc
Lorenzo Pappalardo <lorenzopappalardo7@gmail.com>
2022-11-06 19:23:28 +0100

    Refactored types

--- models/api/Entity.ts
+++ models/api/Entity.ts
@@ -1,20 +1,19 @@
-export interface Channel {
+interface Base {
   title: string;
   link: string;
   description: string;
   pictureURL: string;
-  subscribers: number;
 }
 
-export interface Group {
-  title: string;
-  link: string;
-  description: string;
-  pictureURL: string;
+export type Channel = Base & {
+  subscribers: number;
+};
+
+export type Group = Base & {
   members: number;
   code: string;
   mz_code?: string;
-}
+};
 
 export interface Bot {}
 
