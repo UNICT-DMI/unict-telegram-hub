b58ef1709a32affbbd15ddae844d7a66e1ae2403
Lorenzo Pappalardo <lorenzopappalardo7@gmail.com>
2023-05-01 12:53:04 +0200

    Added next-pwa and configuration files for the PWA

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
+const prod = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-}
+  swcMinify: true
+};
 
-module.exports = nextConfig
+const withPWA = require('next-pwa')({
+  disable: prod ? false : true,
+  dest: 'public',
+  sw: 'service-worker.js'
+});
+
+module.exports = withPWA(nextConfig);
