cf25b8e2af427b2ab4acfd44ea24a6a0dd4a5bf8
Lorenzo Pappalardo <lorenzopappalardo7@gmail.com>
2024-07-14 11:07:46 +0200

    Optimised rendering and added text when no card matches the search

--- app/page.tsx
+++ app/page.tsx
@@ -1,59 +1,71 @@
 'use client';
 
 import { Entity, entities } from '@/app/models';
-import { Box, Skeleton, useTheme } from '@mui/material';
-import { useContext, useEffect, useState } from 'react';
-import { CategoryContext, SearchContext } from './main';
+import { Box, Skeleton, Typography, useTheme } from '@mui/material';
+import { useCallback, useContext, useEffect, useMemo, useState } from 'react';
 import { loadCards } from './actions';
 import GenericCard from './card';
+import { CategoryContext, SearchContext } from './main';
 
 export default function TelegramDashboard() {
-  const [loading, setLoading] = useState<boolean>(false);
-  const [data, setData] = useState<ReadonlyArray<Entity>>();
+  const [loading, setLoading] = useState<boolean>(true);
+  const [data, setData] = useState<ReadonlyArray<Entity>>([]);
   const theme = useTheme();
   const category = useContext(CategoryContext);
   const searchValue = useContext(SearchContext);
 
+  const fetchData = useCallback(
+    async (chosenEntityType: (typeof entities)[number]) => {
+      setLoading(true);
+      setData(await loadCards(chosenEntityType, searchValue));
+      setLoading(false);
+    },
+    [searchValue]
+  );
+
   useEffect(() => {
     fetchData(category);
-  }, [category]);
+  }, [fetchData, category]);
 
-  async function fetchData(chosenEntityType: (typeof entities)[number]) {
-    setLoading(true);
-    setData(await loadCards(chosenEntityType));
-    setLoading(false);
-  }
-
-  function listEntities() {
+  const cards = useMemo(() => {
     if (loading) {
       return new Array(20)
         .fill(undefined)
         .map((_, i) => <Skeleton key={i} variant="rectangular" height="160px" animation="pulse" />);
     }
 
-    if (data !== undefined) {
-      return data.map(
-        (entity, i) =>
-          (searchValue.length === 0 ||
-            entity.title.toLowerCase().includes(searchValue.toLowerCase())) && (
-            <GenericCard
-              key={`${entity.title}${i}`}
-              isLeaderboard={searchValue.length === 0}
-              entity={{ ...entity, position: i + 1 }}
-            />
-          )
-      );
-    }
-  }
+    return data.map(
+      (entity, i) =>
+        (searchValue.length === 0 ||
+          entity.title.toLowerCase().includes(searchValue.toLowerCase())) && (
+          <GenericCard
+            key={`${entity.title}${i}`}
+            isLeaderboard={searchValue.length === 0}
+            entity={{ ...entity, position: i + 1 }}
+          />
+        )
+    );
+  }, [loading, data, searchValue]);
 
-  return (
-    <Box
-      display="grid"
-      gridTemplateColumns="repeat(auto-fill, minmax(min(25em, 100%), 1fr))"
-      gap={2}
-      margin={1}
-      bgcolor={theme.palette.background.default}>
-      {listEntities()}
-    </Box>
-  );
+  if (cards.length > 0) {
+    return (
+      <Box
+        display="grid"
+        gridTemplateColumns="repeat(auto-fill, minmax(min(25em, 100%), 1fr))"
+        gap={2}
+        margin={1}
+        bgcolor={theme.palette.background.default}>
+        {cards}
+      </Box>
+    );
+  } else {
+    return (
+      <Box textAlign="center">
+        <Typography fontSize="2rem">
+          {searchValue.length > 0 ? `No matches for "${searchValue}"` : 'No data'}
+        </Typography>
+        <Typography fontSize="3rem">😕</Typography>
+      </Box>
+    );
+  }
 }
